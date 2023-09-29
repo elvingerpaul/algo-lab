6f9a2d0620ae5e670979eec160c0924677f2f273
Paul Elvinger <Paul@student-net-hg-2766.intern.ethz.ch>
2023-09-29 10:38:43 +0200

    complete dominoes

---
+++ problems/week-01/dominoes/dominoes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void run_testcase()
+{
+    int n;
+    cin >> n;
+    int max = 1, dominoe = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> dominoe;
+        
+        if (i < max && i + dominoe > max)
+            max = i + dominoe;
+    }
+
+    cout << min(n, max) << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        run_testcase();
+    }
+
+    return 0;
+}
