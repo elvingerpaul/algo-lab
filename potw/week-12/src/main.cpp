506c0c96ab12a5ef7fa0522539cb3a4ca5ec7da6
Paul Elvinger <elvingerpa@gmail.com>
2024-02-01 11:23:56 +0100

    redo pied-piper

--- potw/week-12/src/main.cpp
+++ potw/week-12/src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <vector>
 
-typedef std::pair<int, int> tint;
-typedef std::vector<tint> vtint;
-typedef std::vector<vtint> adj;
+struct Edge
+{
+    int succ;
+    int rats;
+};
+
+typedef std::vector<Edge> vEdge;
+typedef std::vector<vEdge> adj;
 typedef std::vector<int> vint;
 typedef std::vector<vint> mint;
 
@@ -11,62 +16,63 @@ mint dp;
 adj forward;
 adj backward;
 
-int solve(int fwd, int bwd, int n)
+int solve(int up, int down, int n)
 {
-    if (fwd == n - 1 && bwd == n - 1)
-        return 0;
-
-    if (fwd == bwd && fwd != 0) // exclude the start case
-        return -1;
-
-    if (dp[fwd][bwd] != -2)
-        return dp[fwd][bwd];
+    if (dp[up][down] != -2)
+        return dp[up][down];
 
     int best = -1;
-    if (fwd < bwd)
-    {
-        // advance foward pointer towards n-1 if possible
-        for (auto f : forward[fwd])
+    if (up < down)
+    { // advance up towards n-1
+        for (Edge e : forward[up])
         {
-            int child_sol = solve(f.first, bwd, n);
-            if (child_sol >= 0) // solution < 0 means there is no path from child to n-1
-                best = std::max(best, child_sol + f.second);
+            if (e.succ == down && down != n - 1) // not allowed to use same place twice except n-1
+                continue;
+
+            int num_rats = solve(e.succ, down, n);
+            if (num_rats >= 0) // if result < 0, no valid path up to n-1
+                best = std::max(best, num_rats + e.rats);
         }
     }
     else
-    {
-        // advance backward pointer towards n-1 if possible
-        for (auto b : backward[bwd])
+    { // advance down towards n-1
+        for (Edge e : backward[down])
         {
-            int child_sol = solve(fwd, b.first, n);
-            if (child_sol >= 0) // solution < 0 means there is no path from child to n-1
-                best = std::max(best, child_sol + b.second);
+            if (e.succ == up && up != n - 1) // not allowed to use same place twice except n-1
+                continue;
+
+            int num_rats = solve(up, e.succ, n);
+            if (num_rats >= 0) // if result < 0, no valid paht up to n-1
+                best = std::max(best, num_rats + e.rats);
         }
     }
 
-    dp[fwd][bwd] = best;
-
+    dp[up][down] = best;
     return best;
 }
 
 void testcase()
 {
-    int n, m; std::cin >> n >> m;
+    int n, m;
+    std::cin >> n >> m;
 
     forward = adj(n);
     backward = adj(n);
-
     for (int i = 0; i < m; i++)
     {
         int u, v, f; std::cin >> u >> v >> f;
         if (u < v)
-            forward[u].push_back(std::make_pair(v, f));
+            forward[u].push_back({v, f});
         else
-            backward[v].push_back(std::make_pair(u, f));
+            backward[v].push_back({u, f});
     }
-
+    // dp[up][down] = max number of rats that can be caught when starting in up,
+    // going all the way up to n-1 and ending in down
+    // dp[up][down] = -2 -> value not calculated yet
+    // dp[up][down] = -1 -> no valid path leading up to n-1
     dp = mint(n, vint(n, -2));
-    std::cout << solve(0, 0, n) << std::endl;
+    dp[n - 1][n - 1] = 0;
+    std::cout << solve(0, 0, n) << "\n";
 }
 
 int main()
